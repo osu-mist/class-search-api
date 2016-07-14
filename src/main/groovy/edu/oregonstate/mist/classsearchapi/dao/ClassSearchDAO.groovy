53dcf38e102787d6ac4f5a7dbc9cfe80a534e80a
floppyears <cedenoj@onid.oregonstate.edu>
2016-07-14 16:20:11 -0700

    Refactor dao for reusability
    CO-440

--- src/main/groovy/edu/oregonstate/mist/classsearchapi/dao/ClassSearchDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/classsearchapi/dao/ClassSearchDAO.groovy
@@ -6,41 +6,23 @@ import edu.oregonstate.mist.api.jsonapi.ResourceObject
 import edu.oregonstate.mist.classsearchapi.core.Attributes
 import edu.oregonstate.mist.classsearchapi.core.Faculty
 import edu.oregonstate.mist.classsearchapi.core.MeetingTime
-import groovyx.net.http.HTTPBuilder
 import org.apache.http.HttpEntity
-import org.apache.http.HttpHeaders
-import org.apache.http.HttpHost
-import org.apache.http.auth.AuthScope
-import org.apache.http.auth.UsernamePasswordCredentials
-import org.apache.http.client.AuthCache
-import org.apache.http.client.CredentialsProvider
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.CloseableHttpResponse
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.protocol.HttpClientContext
-import org.apache.http.client.utils.URIBuilder
-import org.apache.http.impl.auth.BasicScheme
-import org.apache.http.impl.client.BasicAuthCache
-import org.apache.http.impl.client.BasicCredentialsProvider
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-
-import static groovyx.net.http.ContentType.JSON
 
 class ClassSearchDAO {
     private static final String STATUS_CLOSED = 'Closed'
     private static final String STATUS_OPEN = 'Open'
     private static final String STATUS_WAITLISTED = 'Waitlisted'
 
-    private final Map<String, String> classSearchConfiguration
-
+    private UtilHttp utilHttp
     private HttpClient httpClient
     private ObjectMapper mapper = new ObjectMapper()
 
     ClassSearchDAO(Map<String, String> classSearchConfiguration, HttpClient httpClient) {
-        this.classSearchConfiguration = classSearchConfiguration
         this.httpClient = httpClient
+        this.utilHttp = new UtilHttp(classSearchConfiguration)
     }
 
     /**
@@ -56,29 +38,15 @@ class ClassSearchDAO {
      */
     public def getData(String term, String subject, String courseNumber, String q,
                        Integer pageNumber, Integer pageSize) {
+        CloseableHttpResponse response
         def data = []
-        def query = getQueryMap(term, subject, courseNumber, q, pageNumber, pageSize)
         def sourcePagination = [totalCount: 0, pageOffset: 0, pageMaxSize: 0]
 
-        CredentialsProvider credsProvider = new BasicCredentialsProvider()
-        credsProvider.setCredentials(
-                new AuthScope(backendHost, backendPort),
-                new UsernamePasswordCredentials(backendUsername, backendPassword))
-
-        HttpClientContext context = HttpClientContext.create()
-        context.setCredentialsProvider(credsProvider)
-
-        URI uri = getBackendURI(query)
-
-        HttpGet httpGet = new HttpGet(uri)
-        httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-        httpGet.setHeader(HttpHeaders.ACCEPT, "application/json")
-
-        CloseableHttpResponse response = httpclient.execute(httpGet, context)
-
         try {
-            HttpEntity entity = response.getEntity()
+            def query = getQueryMap(term, subject, courseNumber, q, pageNumber, pageSize)
+            response = utilHttp.sendGet(query, httpClient)
 
+            HttpEntity entity = response.getEntity()
             def entityString = EntityUtils.toString(entity)
 
             data = this.mapper.readValue(entityString,
@@ -86,28 +54,12 @@ class ClassSearchDAO {
                 });
 
             sourcePagination = getSourcePagination(response.getAllHeaders())
-
             EntityUtils.consume(entity)
         } finally {
-            response.close()
+            response?.close()
         }
 
         [data: getFormattedData(data), sourcePagination: sourcePagination]
-    }
-
-    private URI getBackendURI(LinkedHashMap<String, Integer> query) {
-        URIBuilder uriBuilder = new URIBuilder()
-                .setScheme(backendScheme)
-                .setHost(backendHost)
-                .setPort(backendPort)
-                .setPath(backendPath)
-
-        query.each { k, v ->
-            uriBuilder.setParameter(k, v.toString())
-        }
-
-        URI uri = uriBuilder.build()
-        uri
     }
 
     private static getSourcePagination(headers) {
@@ -247,27 +199,5 @@ class ClassSearchDAO {
         query
     }
 
-    private String getBackendHost() {
-        classSearchConfiguration.get("backendHost")
-    }
 
-    private String getBackendScheme() {
-        classSearchConfiguration.get("backendScheme")
-    }
-
-    private Integer getBackendPort() {
-        classSearchConfiguration.get("backendPort").toInteger()
-    }
-
-    private String getBackendPath() {
-        classSearchConfiguration.get("backendPath")
-    }
-
-    private String getBackendUsername() {
-        classSearchConfiguration.get("backendUsername")
-    }
-
-    private String getBackendPassword() {
-        classSearchConfiguration.get("backendPassword")
-    }
 }
